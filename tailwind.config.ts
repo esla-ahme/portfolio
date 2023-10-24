36ff1089c1d981087e84f8b751af0cde9a19959c
Eslam <eslamahmed9861@gmail.com>
2023-10-24 16:35:03 +0300

    configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,49 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    maxWidth: {
+      container: "1440px",
+      contentContainer: "1140px",
+      containerSmall: "1024px",
+      containercs: "768px",
+    },
     extend: {
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        monteserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        main: "#00796b",
+        bgDark: "#0A1a26",
+        bgLight: "#e3f1f1",
+        textLight: "#c9d9e9",
+        textDark: "#8892b0",
+      },
+      boxShadow: {
+        "3xl": "0 10px 30px -10px rgba(2, 12, 27, 0.5)",
       },
     },
   },
   plugins: [],
-}
+};
 export default config
