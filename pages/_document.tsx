ab6f7ee7dc8f4e7a3844abc90667cf2c9da3e7ff
Eslam <eslamahmed9861@gmail.com>
2023-10-27 16:16:29 +0200

    meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,20 +8,20 @@ export default function Document() {
         <meta name="title" content="Eslam Ahmed - Software Engineer" />
         <meta
           name="description"
-          content="Frontend Engineer specializing in React, Vue, GraphQL, and more."
+          content="Eslam Ahmed - Web Developer Portfolio. Over a year of web development experience specializing in creating web platforms, optimizing UI/UX, and ensuring top-notch SEO. Explore my work and experience in React, Next.js, TypeScript, Tailwind CSS, and Framer Motion."
         />
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://eslam-ahmed.vercel.app/" />
         <meta property="og:title" content="Eslam Ahmed - Software Engineer" />
+        <meta property="og:image" content="/images/cover.png" />{" "}
         <meta
           property="og:description"
-          content="Frontend Engineer specializing in React, Vue, GraphQL, and more."
+          content="An experienced web developer passionate about creating accessible and inclusive web applications. With over a year of expertise in React, Vue, and their ecosystem, Eslam specializes in optimizing websites for a top-notch UI/UX and solid SEO. Dive into the details of a developer dedicated to user satisfaction and engagement."
         />
-        <meta property="og:image" content="/images/eslam.webp" />{" "}
         {/* Reference to the image in the public folder */}
         {/* Twitter */}
-        <meta property="twitter:card" content="/images/eslam.webp" />
+        <meta property="twitter:card" content="/images/cover.png" />
         <meta
           property="twitter:url"
           content="https://eslam-ahmed.vercel.app/"
@@ -32,9 +32,9 @@ export default function Document() {
         />
         <meta
           property="twitter:description"
-          content="Frontend Engineer specializing in React, Vue, GraphQL, and more."
+          content="An experienced web developer passionate about creating accessible and inclusive web applications. With over a year of expertise in React, Vue, and their ecosystem, Eslam specializes in optimizing websites for a top-notch UI/UX and solid SEO. Dive into the details of a developer dedicated to user satisfaction and engagement."
         />
-        <meta property="twitter:image" content="/images/eslam.jpg" />
+        <meta property="twitter:image" content="/images/cover.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
