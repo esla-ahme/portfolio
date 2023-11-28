e39667330a5a562f951a5af7987270e7dcf7911c
Eslam <eslamahmed9861@gmail.com>
2023-11-28 18:21:31 +0200

    smaller burger icon

--- components/Navbar/icon.tsx
+++ components/Navbar/icon.tsx
@@ -9,7 +9,7 @@ type IconProps = {
 const icon = (props: IconProps) => {
   // Your component logic here
 
-  return <Spin size={30} toggled={props.opened} onToggle={props.onClick} />;
+  return <Spin size={28} toggled={props.opened} onToggle={props.onClick} />;
 };
 
 export default icon;
